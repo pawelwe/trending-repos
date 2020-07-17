8615b5cadf5227cede3f205e484d43c2fa51dcc7
Pawel <pawel@MacBook-Pro-Pawel.local>
2020-07-17 20:26:11 +0200

    some styling

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Loader } from '../Loader/Loader';
+import styles from './Messages.scss';
 
 export const Messages = ({ isBusy, error, noResults }) => {
   return (
     <div className="container">
       {isBusy && <Loader />}
-      {error && <span>Something went wrong :(</span>}
-      {noResults && !isBusy && <span>No results</span>}
+      {error && (
+        <span className={styles['error']}>Something went wrong 😔</span>
+      )}
+      {noResults && !isBusy && <span>No results 😔</span>}
     </div>
   );
 };
